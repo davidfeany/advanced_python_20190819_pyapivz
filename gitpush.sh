df20e6742a353db9e1cca0951beff3435cb50eac
David Feany <david.feany@gmail.com>
2019-08-21 00:22:53 -0400

    Wed Aug 21 00:22:53 EDT 2019

--- gitpush.sh
+++ gitpush.sh
@@ -2,6 +2,6 @@
 git add *
 #git config --global user.email "david.feany@gmail.com"
 #git config --global user.name "David Feany"
-#git commit -m "Start of day 2"
+date | xargs -I {} git commit -m "{}"
 git push https://github.com/davidfeany/advanced_python_20190819_pyapivz master
 
